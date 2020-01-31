d95a5853018edad0b3e4d4a1a0f68289295676d5
unknown <->
2020-01-31 23:08:17 +0100

    Init commit. WIP.

---
+++ godot/inventory/interfaces/IDestructible.gd
@@ -0,0 +1,11 @@
+
+
+func _ready():
+	pass # Replace with function body.
+
+func TakeDamage(amount):
+	return false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
---
+++ godot/inventory/models/ItemModel.gd
@@ -0,0 +1,14 @@
+extends "res://inventory/interfaces/IDestructible.gd"
+
+var durability = 10
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func TakeDamage(amount):
+	pass
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
