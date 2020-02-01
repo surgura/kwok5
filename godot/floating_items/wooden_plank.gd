2ed0547c192c8a428b546559a791de95128595c1
Aart Stuurman <aart@astuurman.com>
2020-02-01 14:48:52 +0100

    Raft will add caught items to inventory.

--- godot/floating_items/wooden_plank.gd
+++ godot/floating_items/wooden_plank.gd
@@ -1,4 +1,7 @@
 extends "res://floating_items/floating_item.gd"
 
-func get_base_damage() -> float:
-	return 0.0
+func can_pickup(inventory : Object) -> bool:
+	return true
+
+func get_item():
+	return "wooden_plank"
