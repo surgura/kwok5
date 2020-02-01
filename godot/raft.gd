6a67068277b0fc188089a5e5fab55e57d03ffd8a
Aart Stuurman <aart@astuurman.com>
2020-02-01 11:17:50 +0100

    Temporarily disable item catch callbacks

--- godot/raft.gd
+++ godot/raft.gd
@@ -2,6 +2,7 @@ extends RigidBody2D
 
 var hook_scene = preload("res://hook.tscn")
 var hook_instance = null
+export(NodePath) var inventory_path
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,7 +18,9 @@ func _process(_delta):
 
 
 func _on_raft_body_shape_entered(_body_id, body, _body_shape, _local_shape):
-	var item = body.maybe_get_item([]) # TODO
-	var damage = body.get_damage(self.mass, self.get_linear_velocity(), []) # TODO
-	body.on_hit_raft()
-	
+	pass
+	#var inventory = get_node(inventory_path)
+	#var item = body.maybe_get_item(inventory)
+	# TODO add item to inventory
+	#var damage = body.get_damage(self.mass, self.get_linear_velocity(), []) # TODO
+	#body.on_hit_raft()
