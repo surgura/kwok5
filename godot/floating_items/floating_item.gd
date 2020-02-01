1c647d85fc72e675983560201788bec5cd529afc
Aart Stuurman <aart@astuurman.com>
2020-02-01 23:38:13 +0100

    Fixed double pickup bug

--- godot/floating_items/floating_item.gd
+++ godot/floating_items/floating_item.gd
@@ -1,12 +1,13 @@
 extends RigidBody2D
 
-enum DestroyTrigger {
+enum Trigger {
 	IMPACT,
 	CATCH,
 	NEVER	
 }
 
-export(DestroyTrigger) var destroy_trigger = DestroyTrigger.IMPACT
+export(Trigger) var destroy_trigger = Trigger.IMPACT
+export(Trigger) var pickup_trigger = Trigger.CATCH
 export(bool) var can_release_hook : bool = false
 export(bool) var is_being_reeled : bool = false
 
@@ -20,7 +21,7 @@ func _process(_delta):
 func get_damage(ship_weight : float, ship_velocity : Vector2, inventory : Object) -> float:
 	return get_weight_damage_factor(ship_weight) * get_speed_damage_factor((ship_velocity - get_linear_velocity()).length()) * get_base_damage()
 
-# Default weight damage factor. Function starting at 1 with asymptote at 2.
+# Default weight damage factor. Function sta4rting at 1 with asymptote at 2.
 func get_weight_damage_factor(ship_weight : float) -> float:
 	return 1 + (1 - exp(-ship_weight))
 
@@ -30,22 +31,17 @@ func get_speed_damage_factor(impact_speed : float) -> float:
 
 # Base damage.
 func get_base_damage():
-	return 1
+	return 0
 
 # What should happen when the item collides with the raft.
 func on_hit_raft() -> void:
-	if destroy_trigger == DestroyTrigger.IMPACT or (destroy_trigger == DestroyTrigger.CATCH and is_being_reeled):
+	if destroy_trigger == Trigger.IMPACT or (destroy_trigger == Trigger.CATCH and is_being_reeled):
 		self.queue_free()
 	
 # Returns an inventory item or null.
 func maybe_get_item(inventory : Object):
-	if (!can_pickup(inventory)):
-		return null
-	
-	return get_item()
-
-func can_pickup(inventory : Object) -> bool:
-	return false
+	if pickup_trigger == Trigger.IMPACT or (pickup_trigger == Trigger.CATCH and is_being_reeled):
+		return get_item()
 
 func get_item():
 	return null
--- godot/floating_items/wooden_plank.gd
+++ godot/floating_items/wooden_plank.gd
@@ -1,7 +1,4 @@
 extends "res://floating_items/floating_item.gd"
 
-func can_pickup(inventory : Object) -> bool:
-	return true
-
 func get_item():
 	return "wooden_plank"
