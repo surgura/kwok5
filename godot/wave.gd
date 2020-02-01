3786e7d7db0ba66b8437c479f1c5b05b6acb71c3
Aart Odding <aart@re-guest-195-169-173-131.wireless.uva.nl>
2020-02-01 20:28:10 +0100

    still wet

--- godot/wave.gd
+++ godot/wave.gd
@@ -1,25 +1,15 @@
 extends Node2D
 
 export(float) var waveHeight = 0
-
-export(int) var worldIndexX
-export(int) var worldIndexY
-export(float) var worldPosX
-export(float) var worldPosY
-
-var random = RandomNumberGenerator.new()
+export(Vector2) var basePosition
 
 
-func updateSprite():
+func setup(var random:RandomNumberGenerator):
 	var sprite = get_node("AnimatedSprite")
 	var maxIndex = sprite.frames.get_frame_count("default")
-	var randomSeed = worldIndexX % 300 + ((79 + worldIndexY) % 311) * 300
-	random.set_seed(randomSeed)
 	var spriteIndex = random.randi_range(0, maxIndex)
 	var spriteFlipped = bool(random.randi_range(0, 1))
-	if (sprite.frame != spriteIndex):
-		sprite.frame = spriteIndex
-	if (sprite.flip_h != spriteFlipped):
-		sprite.flip_h = spriteFlipped
+	sprite.frame = spriteIndex
+	sprite.flip_h = spriteFlipped
 
 
