5130ac351ed3a2eec4af9c141db17592b892a427
Aart Stuurman <aart@astuurman.com>
2020-02-01 12:01:10 +0100

    Formatting

--- godot/raft.gd
+++ godot/raft.gd
@@ -13,7 +13,6 @@ func _process(_delta):
 	if Input.is_action_just_pressed("throw_hook"):
 		interact_hook()
 
-
 func _on_raft_body_shape_entered(_body_id, body, _body_shape, _local_shape):
 	var inventory = get_node(inventory_path)
 	#var item = body.maybe_get_item(inventory)
