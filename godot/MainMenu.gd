f60a219ff34c54486fed5a47bbddcc43f2759b14
AartOdding <aart_odding@hotmail.com>
2020-02-02 14:43:21 +0100

    Updated main menu with credits

--- godot/MainMenu.gd
+++ godot/MainMenu.gd
@@ -17,3 +17,20 @@ func _on_button_mouse_entered():
 
 func _on_button_mouse_exited():
 	get_node("playButton/TextureRect").visible = false
+
+
+func _on_creditsButton_pressed():
+	get_node("credits").visible = true
+	print("Created by: ...")
+
+
+func _on_creditsButton_mouse_entered():
+	get_node("creditsButton/TextureRect").visible = true
+
+
+func _on_creditsButton_mouse_exited():
+	get_node("creditsButton/TextureRect").visible = false
+
+
+func _on_Button_pressed():
+	get_node("credits").visible = false
