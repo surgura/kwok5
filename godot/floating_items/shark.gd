266ab078260108a3feb4d1278193c0e62e6a530c
Aart Stuurman <aart@astuurman.com>
2020-02-02 12:58:25 +0100

    Set sprites

--- godot/floating_items/shark.gd
+++ godot/floating_items/shark.gd
@@ -1,7 +1,8 @@
 extends "res://floating_items/floating_item.gd"
 
 var sprites = [
-	preload("res://images/shark.png")
+	preload("res://images/shark.png"),
+	preload("res://images/octopus.png")
 ]
 
 func _ready():
