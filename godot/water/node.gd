0e582f36e029243f970564f1fc61a227abb09d2c
Aart Stuurman <aart@astuurman.com>
2020-02-07 23:21:41 +0100

    Water with image

--- godot/water/node.gd
+++ godot/water/node.gd
@@ -4,8 +4,7 @@ var _neighbours
 var _force
 var _velocity: float = 0.0
 var spring_constant: float = 20.0
-var spring_constant_base: float = 20.0
-var damp = 2.0
+var damp = 1.0
 
 func _init(start_height: float) -> void:
 	self.height = start_height
@@ -18,7 +17,6 @@ func prepare(_delta: float) -> void:
 	self._force = 0
 	for neighbour in self._neighbours:
 		self._force += self.spring_constant * (neighbour.height - self.height)
-	self._force += self.spring_constant_base * (0.0 - self.height)
 	
 func assign(delta: float) -> void:
 	self._velocity += delta * self._force / self.mass
--- godot/water/water.gd
+++ godot/water/water.gd
@@ -2,9 +2,11 @@ extends Node2D
 
 var Node = preload("node.gd")
 var node_grid
-export(int) var width: int = 20
-export(int) var height: int = 20
+export(int) var width: int = 30
+export(int) var height: int = 30
 var dist = 50
+
+var texture = preload("res://images/wave1.png")
 
 func _ready():
 	self.node_grid = []
@@ -24,15 +26,20 @@ func _ready():
 			if x != width-1:
 				self.node_grid[y][x].add_neighbour(self.node_grid[y][x+1])
 			
-	#self.node_grid[0][0].height = 10.0
-			
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("makewave"):
 		var relpos = get_global_mouse_position() - global_position
 		relpos += Vector2(dist/2, dist/2)
-		var gridpos = Vector2(int(relpos.x / self.dist), int(relpos.y / self.dist))
-		#print(gridpos)
-		self.node_grid[gridpos.y][gridpos.x].height -= 10
+		var gridpos = Vector2(int(relpos.x / self.dist), int(relpos.y / self.dist * 2.0))
+		if gridpos.x < 1:
+			gridpos.x = 1
+		if gridpos.x >= width-1:
+			gridpos.x = width-2
+		if gridpos.y < 1:
+			gridpos.y = 1
+		if gridpos.y >= height-1:
+			gridpos.y = height-2
+		self.node_grid[gridpos.y][gridpos.x].height -= 200
 	
 	for y in range(0, height):
 		for x in range(0, width):
@@ -43,6 +50,7 @@ func _process(delta: float) -> void:
 	update()
 			
 func _draw():
-	for y in range(0, height):
-		for x in range(0, width):
-			draw_circle(Vector2(x*self.dist, y*self.dist), 10+5*self.node_grid[y][x].height, Color(1,1,1))
+	for y in range(0, height / 3):
+		for x in range(0, width / 2):
+			#draw_circle(Vector2(x*self.dist, y*self.dist), 10+1*self.node_grid[y][x].height, Color(0.5,0.5,0.5))
+			draw_texture(texture, Vector2(x * 2 *self.dist - texture.get_size().x / 2, y*3*self.dist / 2 - self.node_grid[y*3][x*2].height))
